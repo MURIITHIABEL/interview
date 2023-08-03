c5365dcce2f040a4a2397e4e5a2a3966da9870cb
Abel Muriithi <abelmuriithi04@gmail.com>
2023-08-03 23:23:35 +0300

    troubleshooting

--- frontend/src/components/FormData.js
+++ frontend/src/components/FormData.js
@@ -39,7 +39,7 @@ const Patient = () => {
         relationship: kinRelationship,
       },
     };
-
+  
     // Save the data to the database
     fetch('http://localhost:9292/patients', {
       method: 'POST',
@@ -51,29 +51,26 @@ const Patient = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log('Form submitted!', data);
-
-        
+  
         const message = Object.entries(formData).map(([key, value]) => {
           return `${key}: ${value}`;
         }).join('\n');
-
+  
         const templateParams = {
           to_email: formData.email, 
-          subject:  formData.name,
+          subject: formData.name,
           message: formData.name,
           telephone: formData.telephone,
           address: formData.address,
         };
-
-     
+  
         emailjs.send("service_wt2d74l", "template_sn6n67c", templateParams, "idTLnUqMsdu3F0IAc")
           .then((response) => {
             console.log('Email sent successfully!', response);
-            window.location.reload();
+            window.location.reload(); // Reload the page after successful email sending
           })
           .catch((error) => {
             console.error('Error sending email:', error);
-            window.location.reload();
           });
       })
       .catch((error) => {
@@ -82,165 +79,191 @@ const Patient = () => {
   };
   
   
+  
 
   
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="telephone">Telephone:</label>
-        <input
-          type="text"
-          id="telephone"
-          value={telephone}
-          onChange={(e) => setTelephone(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="dob">Date of Birth:</label>
-        <input
-          type="date"
-          id="dob"
-          value={dob}
-          onChange={(e) => setDob(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="idNumber">ID Number:</label>
-        <input
-          type="number"
-          id="idNumber"
-          value={idNumber}
-          onChange={(e) => setIdNumber(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="address">Address:</label>
-        <textarea
-          id="address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="county">County:</label>
-        <input
-          type="text"
-          id="county"
-          value={county}
-          onChange={(e) => setCounty(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="subCounty">Sub County:</label>
-        <input
-          type="text"
-          id="subCounty"
-          value={subCounty}
-          onChange={(e) => setSubCounty(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="gender">Gender:</label>
-        <input
-          type="text"
-          id="gender"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="maritalStatus">Marital Status:</label>
-        <input
-          type="text"
-          id="maritalStatus"
-          value={maritalStatus}
-          onChange={(e) => setMaritalStatus(e.target.value)}
-        />
-      </div>
-
-     
-      <h2>Next of Kin</h2>
-
-      <div>
-        <label htmlFor="kinName">Name:</label>
-        <input
-          type="text"
-          id="kinName"
-          value={kinName}
-          onChange={(e) => setKinName(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="kinDob">Date of Birth:</label>
-        <input
-          type="date"
-          id="kinDob"
-          value={kinDob}
-          onChange={(e) => setKinDob(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="kinIdNumber">ID Number:</label>
-        <input
-          type="number"
-          id="kinIdNumber"
-          value={kinIdNumber}
-          onChange={(e) => setKinIdNumber(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="kinGender">Gender:</label>
-        <input
-          type="text"
-          id="kinGender"
-          value={kinGender}
-          onChange={(e) => setKinGender(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <label htmlFor="kinRelationship">Relationship:</label>
-        <input
-          type="text"
-          id="kinRelationship"
-          value={kinRelationship}
-          onChange={(e) => setKinRelationship(e.target.value)}
-        />
-      </div>
-
-      <button type="submit">Submit</button>
-    </form>
+    <form
+    onSubmit={handleSubmit}
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      maxWidth: "400px",
+      margin: "0 auto",
+    }}
+  >
+    <h2>Patients Registration Form</h2>
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="telephone">Telephone:</label>
+      <input
+        type="text"
+        id="telephone"
+        value={telephone}
+        onChange={(e) => setTelephone(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="dob">Date of Birth:</label>
+      <input
+        type="date"
+        id="dob"
+        value={dob}
+        onChange={(e) => setDob(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="idNumber">ID Number:</label>
+      <input
+        type="number"
+        id="idNumber"
+        value={idNumber}
+        onChange={(e) => setIdNumber(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="address">Address:</label>
+      <textarea
+        id="address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="county">County:</label>
+      <input
+        type="text"
+        id="county"
+        value={county}
+        onChange={(e) => setCounty(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="subCounty">Sub County:</label>
+      <input
+        type="text"
+        id="subCounty"
+        value={subCounty}
+        onChange={(e) => setSubCounty(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="gender">Gender:</label>
+      <input
+        type="text"
+        id="gender"
+        value={gender}
+        onChange={(e) => setGender(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="maritalStatus">Marital Status:</label>
+      <input
+        type="text"
+        id="maritalStatus"
+        value={maritalStatus}
+        onChange={(e) => setMaritalStatus(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <h2>Next of Kin</h2>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="kinName">Name:</label>
+      <input
+        type="text"
+        id="kinName"
+        value={kinName}
+        onChange={(e) => setKinName(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="kinDob">Date of Birth:</label>
+      <input
+        type="date"
+        id="kinDob"
+        value={kinDob}
+        onChange={(e) => setKinDob(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="kinIdNumber">ID Number:</label>
+      <input
+        type="number"
+        id="kinIdNumber"
+        value={kinIdNumber}
+        onChange={(e) => setKinIdNumber(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="kinGender">Gender:</label>
+      <input
+        type="text"
+        id="kinGender"
+        value={kinGender}
+        onChange={(e) => setKinGender(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <div style={{ marginBottom: "10px" }}>
+      <label htmlFor="kinRelationship">Relationship:</label>
+      <input
+        type="text"
+        id="kinRelationship"
+        value={kinRelationship}
+        onChange={(e) => setKinRelationship(e.target.value)}
+        style={{ width: "100%" }}
+      />
+    </div>
+  
+    <button type="submit" style={{ width: "100%", padding: "10px" }}>
+      Submit
+    </button>
+  </form>
   );
 };
 
